9d1118e1f6aebfb272a41a0126697ec6b2f623cd
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-03-30 14:40:01 +0300

    init project

---
+++ src/test/java/com/example/javitto/JavittoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javitto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
