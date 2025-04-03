baa4847d0411c43f246361084ad6288e40233058
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-03 16:31:34 +0300

    Добавил advertisementDTO

---
+++ src/main/java/com/example/javitto/DTO/AdvertisementDTO.java
@@ -0,0 +1,27 @@
+package com.example.javitto.DTO;
+
+import com.example.javitto.entity.User;
+import com.example.javitto.entity.enums.ParentCategory;
+import com.example.javitto.entity.enums.SubCategory;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Data
+public class AdvertisementDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private BigDecimal cost;
+    private ParentCategory parentCategory;
+    private SubCategory subCategory;
+    private LocalDateTime dateOfCreation;
+    private String address;
+    private List<String> photoUrl;
+    private UserDTO user;
+}
