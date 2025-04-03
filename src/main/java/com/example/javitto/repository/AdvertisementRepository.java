478d99fb52bd617809e8f3fa1c1678ee2907492e
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-03 17:11:31 +0300

    добавил репозиторий для advertisement

---
+++ src/main/java/com/example/javitto/repository/AdvertisementRepository.java
@@ -0,0 +1,19 @@
+package com.example.javitto.repository;
+
+
+import com.example.javitto.entity.Advertisement;
+import com.example.javitto.entity.enums.City;
+import com.example.javitto.entity.enums.ParentCategory;
+import com.example.javitto.entity.enums.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
+    Optional<Advertisement> findByCity(City city);
+    Optional<Advertisement> findByTitle(String title);
+    Optional<Advertisement> findByParentCategory(ParentCategory parentCategory);
+    Optional<Advertisement> findByParentCategoryAndSubCategory(ParentCategory parentCategory, SubCategory subCategory);
+}
