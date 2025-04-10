a746537c2604ccea2a39dc65cc1773dfa2e10d8f
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-10 21:30:33 +0300

    конфиг для mail-sender

--- src/main/java/com/example/javitto/DTO/response/NotificationResponse.java
+++ src/main/java/com/example/javitto/DTO/response/NotificationResponse.java
@@ -1,11 +1,11 @@
-package com.example.javitto.DTO;
+package com.example.javitto.DTO.response;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class NotificationDTO {
+public class NotificationResponse {
     private String recipient;
     private String subject;
     private String text;
