4fcde0f324895ea1ba3fac7d57d40b9461a542b3
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-11 21:55:47 +0300

    Добавил метод для отправки уведомления на почту после создания объявления

--- src/main/java/com/example/javitto/service/EmailNotificationService.java
+++ src/main/java/com/example/javitto/service/EmailNotificationService.java
@@ -3,6 +3,7 @@ package com.example.javitto.service;
 import com.example.javitto.DTO.NotificationDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -32,12 +33,21 @@ public class EmailNotificationService implements NotificationService {
         NotificationDTO notification = new NotificationDTO();
         notification.setRecipient(email);
         notification.setSubject("Добро пожаловать в Javitto!");
-        notification.setText("Здравствуйте,    " + username +
+        notification.setText("Здравствуйте, " + username +
                 "Ваша регистрация успешно завершена. Добро пожаловать на нашу платформу Javitto!");
 
         send(notification);
         log.info("Отправка уведомления после регистрации пользователю : {}", username);
     }
 
+    public void sendAdvertisementEmail(String email, String title, String username) {
+        NotificationDTO notification = new NotificationDTO();
+        notification.setRecipient(email);
+        notification.setSubject("Спасибо за то что пользуетесь Javitto!");
+        notification.setText("Привет, " + username + "! Вы выставили объявление на продажу " + title);
+
+        send(notification);
+        log.info("Отправка уведомления после регистрации пользователю : {}", username);
+    }
 
 }
