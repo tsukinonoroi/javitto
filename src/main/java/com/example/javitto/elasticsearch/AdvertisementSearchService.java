f6635b4d2658a29447a1ee7e0f55d56d8c481837
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-18 22:21:18 +0300

    сервис поиска

---
+++ src/main/java/com/example/javitto/elasticsearch/AdvertisementSearchService.java
@@ -0,0 +1,24 @@
+package com.example.javitto.elasticsearch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class AdvertisementSearchService {
+    private final AdvertisementSearchRepository searchRepository;
+
+    public List<AdvertisementDocument> search(String query) {
+       return searchRepository.findByTitleContainingOrDescriptionContaining(query, query);
+    }
+
+    public void saveToIndex(AdvertisementDocument doc) {
+        searchRepository.save(doc);
+    }
+
+    public void deleteFromIndex(Long id) {
+        searchRepository.deleteById(id);
+    }
+}
