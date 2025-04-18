3dce4f8a7f87555e77b80ff9cfe143dfe84a5912
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-18 21:21:44 +0300

    апдейтнул маппер

--- src/main/java/com/example/javitto/DTO/mapper/AdvertisementMapper.java
+++ src/main/java/com/example/javitto/DTO/mapper/AdvertisementMapper.java
@@ -3,6 +3,7 @@ package com.example.javitto.DTO.mapper;
 import com.example.javitto.DTO.request.AdvertisementCreateRequest;
 import com.example.javitto.DTO.request.AdvertisementUpdateRequest;
 import com.example.javitto.DTO.response.AdvertisementResponse;
+import com.example.javitto.elasticsearch.AdvertisementDocument;
 import com.example.javitto.entity.Advertisement;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -18,5 +19,7 @@ public interface AdvertisementMapper {
     Advertisement toEntity(AdvertisementCreateRequest request);
 
     void updateAdvertisementWithRequest(AdvertisementUpdateRequest request, @MappingTarget Advertisement advertisement);
-
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "description", target = "description")
+    AdvertisementDocument toDocument(Advertisement advertisement);
 }
