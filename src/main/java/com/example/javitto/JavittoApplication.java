627025e9fa247602c92fdeb61b5a537db2c89fc8
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-20 19:53:07 +0300

    добавил в окружение redis

--- src/main/java/com/example/javitto/JavittoApplication.java
+++ src/main/java/com/example/javitto/JavittoApplication.java
@@ -2,7 +2,9 @@ package com.example.javitto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class JavittoApplication {
 
