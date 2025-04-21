5cab1d23a69fbc389122cd8ddbbb1031ac8978b5
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-21 19:54:53 +0300

    закэшировал метод главной стр

--- src/main/java/com/example/javitto/DTO/response/AdvertisementPreviewResponse.java
+++ src/main/java/com/example/javitto/DTO/response/AdvertisementPreviewResponse.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class AdvertisementPreviewResponse {
+public class AdvertisementPreviewResponse implements Serializable {
     private String title;
     private String description;
     private LocalDateTime dateOfCreation;
--- src/main/java/com/example/javitto/elasticsearch/AdvertisementSearchService.java
+++ src/main/java/com/example/javitto/elasticsearch/AdvertisementSearchService.java
@@ -3,6 +3,7 @@ package com.example.javitto.elasticsearch;
 import com.example.javitto.DTO.mapper.AdvertisementMapper;
 import com.example.javitto.DTO.response.AdvertisementPreviewResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -22,8 +23,7 @@ public class AdvertisementSearchService {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "dateOfCreation"));
 
         if (query == null || query.trim().isEmpty()) {
-            Page<AdvertisementDocument> docs = searchRepository.findAll(pageable);
-            return docs.map(mapper::toPreview);
+            getLatest(page, size);
         }
 
         Page<AdvertisementDocument> searchResults = searchRepository
@@ -32,6 +32,7 @@ public class AdvertisementSearchService {
         return searchResults.map(mapper::toPreview);
     }
 
+    @Cacheable(value = "latest_ads", key = "#page + '-' + #size")
     public Page<AdvertisementPreviewResponse> getLatest(int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "dateOfCreation"));
 
