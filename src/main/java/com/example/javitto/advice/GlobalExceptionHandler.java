0982bf8bd037205e083a1f2862b3b188de688e42
tsukinonoroi <seytumerov.e.i21@gmail.com>
2025-04-22 19:53:20 +0300

    доработал advice

--- src/main/java/com/example/javitto/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/javitto/advice/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.javitto.advice;
 
+import co.elastic.clients.elasticsearch.nodes.Http;
 import com.example.javitto.exception.AdvertisementNotFoundException;
+import com.example.javitto.exception.RegistrationException;
 import com.example.javitto.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFound(UserNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(RegistrationException.class)
+    public ResponseEntity<?> handleRegError(RegistrationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("error", ex.getMessage()));
     }
 }
--- src/main/java/com/example/javitto/validator/AdvertisementValidator.java
+++
@@ -1,7 +0,0 @@
-package com.example.javitto.validator;
-
-
-import com.example.javitto.DTO.request.AdvertisementCreateRequest;
-
-public class AdvertisementValidator {
-}
